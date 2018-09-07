407a02cedbc954520919a60ba8f10be6d1c6fc8c
Morgan Breth <mbreth1@gmail.com>
2018-09-07 18:55:22 -0500

    Add files via upload

---
+++ Web browse with filter.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+while True:
+    search=input('Search: ')
+    if search in ('amazon.com', 'games.com'):
+        print('Invalid search criteria. Try again')
+
+    if search not in ('amazon.com', 'games.com'):
+        webbrowser.open('http://'+search)
+    
+
+    
+    
